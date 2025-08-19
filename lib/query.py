40b49d34a117ab686db94429f72aae3c28f770c7
Jeroen van der Heijden <jeroen@cesbit.com>
2025-08-19 15:46:06 +0200

    debug info

--- lib/query.py
+++ lib/query.py
@@ -1,5 +1,6 @@
 import asyncio
 import aiohttp
+import logging
 from typing import Any
 from libprobe.exceptions import CheckException
 from .connector import get_connector
@@ -31,26 +32,32 @@ async def query_devices(asset_config: dict,
         'client_id': client_id,
         'client_secret': client_secret,
     }
+    headers = {
+        'User-Agent': USER_AGENT,
+    }
     uri = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
 
     async with aiohttp.ClientSession(
             timeout=aiohttp_timeout,
             connector=get_connector(loop=loop),
             headers={'User-Agent': USER_AGENT}) as session:
-        async with session.post(uri, data=body, ssl=True) as resp:
+        async with session.post(
+                uri,
+                headers=headers,
+                data=body,
+                ssl=True) as resp:
             data = await resp.json()
+            logging.debug(data)
+            logging.debug(body)
             access_token = data['access_token']
 
     devices = []
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
+    headers['Authorization'] = f'Bearer {access_token}'
     uri = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
     while uri:
         async with aiohttp.ClientSession(
                 timeout=aiohttp_timeout,
-                connector=get_connector(loop=loop),
-                headers={'User-Agent': USER_AGENT}) as session:
+                connector=get_connector(loop=loop)) as session:
             async with session.get(uri, headers=headers, ssl=True) as resp:
                 data = await resp.json()
                 devices.extend(data['value'])
